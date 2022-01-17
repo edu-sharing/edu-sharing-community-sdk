0e4e14220f23c4882c7fdf5a88f086a1b928b740
Frank Thomschke <thomschke@edu-sharing.net>
2022-01-17 12:02:15 +0100

    improve git workflow with worktrees

--- esclone.sh
+++ esclone.sh
@@ -23,13 +23,13 @@ repos+=(services-rendering)
 repos+=(deploy/docker)
 repos+=(sdk)
 
-add() {
+create() {
 	shift || exit 1
 	path="clone/$1"
 
 	mkdir -p "$path"
 	pushd "$path" >/dev/null || exit 1
-	branch="maven/develop"
+	branch="$2"
 	[[ ! -d parent ]] && git clone -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/edu-sharing-community-parent.git parent
 	[[ ! -d bom ]] && git clone -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/edu-sharing-community-bom.git bom
 	[[ ! -d repository ]] && git clone -b "$branch" --recurse-submodules https://scm.edu-sharing.com/edu-sharing/community/repository/edu-sharing-community-repository.git repository
@@ -53,6 +53,36 @@ add() {
 				git submodule init
 				git submodule update --recursive
 			}
+		}
+		popd >/dev/null
+	done
+}
+
+add() {
+	shift || exit 1
+	path="clone/$1"
+
+	mkdir -p "$path"
+	pushd "$path" >/dev/null || exit 1
+	branch="$1"
+	[[ ! -d parent ]] && git clone -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/edu-sharing-community-parent.git parent
+	[[ ! -d bom ]] && git clone -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/edu-sharing-community-bom.git bom
+	[[ ! -d repository ]] && git clone -b "$branch" --recurse-submodules https://scm.edu-sharing.com/edu-sharing/community/repository/edu-sharing-community-repository.git repository
+	[[ ! -d repository-plugin-cluster ]] && git clone -b "$branch" https://scm.edu-sharing.com/edu-sharing/enterprise/repository/edu-sharing-enterprise-repository-plugin-cluster.git repository-plugin-cluster
+	[[ ! -d repository-plugin-elastic ]] && git clone -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/repository/edu-sharing-community-repository-plugin-elastic.git repository-plugin-elastic
+	#[[ ! -d repository-plugin-mongo ]] && git clone -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/repository/edu-sharing-community-repository-plugin-mongo.git repository-plugin-mongo
+	[[ ! -d repository-plugin-remote ]] && git clone -b "$branch" https://scm.edu-sharing.com/edu-sharing/enterprise/repository/edu-sharing-enterprise-repository-plugin-remote.git repository-plugin-remote
+	[[ ! -d repository-plugin-transform ]] && git clone -b "$branch" https://scm.edu-sharing.com/edu-sharing/enterprise/repository/edu-sharing-enterprise-repository-plugin-transform.git repository-plugin-transform
+	[[ ! -d services-rendering ]] && git clone -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/services/edu-sharing-community-services-rendering.git services-rendering
+	[[ ! -d deploy ]] && git clone -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/edu-sharing-community-deploy.git deploy
+	[[ ! -d sdk ]] && git clone -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/edu-sharing-community-sdk.git sdk
+	popd >/dev/null
+
+	for repo in "$path"/*/; do
+		pushd "$repo" >/dev/null || exit 1
+		[[ -f .gitmodules ]] && {
+			git submodule init
+			git submodule update --recursive
 		}
 		popd >/dev/null
 	done
--- esworktree.sh
+++ esworktree.sh
@@ -20,67 +20,74 @@ repos+=(repository-plugin-cluster)
 repos+=(repository-plugin-remote)
 repos+=(repository-plugin-transform)
 repos+=(services-rendering)
-repos+=(deploy/docker)
+repos+=(deploy)
 repos+=(sdk)
 
-[[ ! -d main ]] && {
-	mkdir -p main
-	pushd main >/dev/null || exit 1
-	branch="maven/develop"
-	[[ ! -d parent ]] && git clone -bare -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/edu-sharing-community-parent.git parent
-	[[ ! -d bom ]] && git clone -bare -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/edu-sharing-community-bom.git bom
-	[[ ! -d repository ]] && git clone -bare -b "$branch" --recurse-submodules https://scm.edu-sharing.com/edu-sharing/community/repository/edu-sharing-community-repository.git repository
-	[[ ! -d repository-plugin-cluster ]] && git clone -bare -b "$branch" https://scm.edu-sharing.com/edu-sharing/enterprise/repository/edu-sharing-enterprise-repository-plugin-cluster.git repository-plugin-cluster
-	[[ ! -d repository-plugin-elastic ]] && git clone -bare -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/repository/edu-sharing-community-repository-plugin-elastic.git repository-plugin-elastic
-	#[[ ! -d repository-plugin-mongo ]] && git clone -bare -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/repository/edu-sharing-community-repository-plugin-mongo.git repository-plugin-mongo
-	[[ ! -d repository-plugin-remote ]] && git clone -bare -b "$branch" https://scm.edu-sharing.com/edu-sharing/enterprise/repository/edu-sharing-enterprise-repository-plugin-remote.git repository-plugin-remote
-	[[ ! -d repository-plugin-transform ]] && git clone -bare -b "$branch" https://scm.edu-sharing.com/edu-sharing/enterprise/repository/edu-sharing-enterprise-repository-plugin-transform.git repository-plugin-transform
-	[[ ! -d services-rendering ]] && git clone -bare -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/services/edu-sharing-community-services-rendering.git services-rendering
-	[[ ! -d deploy ]] && git clone -bare -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/edu-sharing-community-deploy.git deploy
-	[[ ! -d sdk ]] && git clone -bare -b "$branch" https://scm.edu-sharing.com/edu-sharing/community/edu-sharing-community-sdk.git sdk
-	popd >/dev/null
-}
+mkdir -p main
+pushd main >/dev/null || exit 1
+[[ ! -d parent ]] && git clone --no-checkout https://scm.edu-sharing.com/edu-sharing/community/edu-sharing-community-parent.git parent
+[[ ! -d bom ]] && git clone --no-checkout https://scm.edu-sharing.com/edu-sharing/community/edu-sharing-community-bom.git bom
+[[ ! -d repository ]] && git clone --no-checkout --recurse-submodules https://scm.edu-sharing.com/edu-sharing/community/repository/edu-sharing-community-repository.git repository
+[[ ! -d repository-plugin-cluster ]] && git clone --no-checkout https://scm.edu-sharing.com/edu-sharing/enterprise/repository/edu-sharing-enterprise-repository-plugin-cluster.git repository-plugin-cluster
+[[ ! -d repository-plugin-elastic ]] && git clone --no-checkout https://scm.edu-sharing.com/edu-sharing/community/repository/edu-sharing-community-repository-plugin-elastic.git repository-plugin-elastic
+#[[ ! -d repository-plugin-mongo ]] && git clone --no-checkout https://scm.edu-sharing.com/edu-sharing/community/repository/edu-sharing-community-repository-plugin-mongo.git repository-plugin-mongo
+[[ ! -d repository-plugin-remote ]] && git clone --no-checkout https://scm.edu-sharing.com/edu-sharing/enterprise/repository/edu-sharing-enterprise-repository-plugin-remote.git repository-plugin-remote
+[[ ! -d repository-plugin-transform ]] && git clone --no-checkout https://scm.edu-sharing.com/edu-sharing/enterprise/repository/edu-sharing-enterprise-repository-plugin-transform.git repository-plugin-transform
+[[ ! -d services-rendering ]] && git clone --no-checkout https://scm.edu-sharing.com/edu-sharing/community/services/edu-sharing-community-services-rendering.git services-rendering
+[[ ! -d deploy ]] && git clone --no-checkout https://scm.edu-sharing.com/edu-sharing/community/edu-sharing-community-deploy.git deploy
+[[ ! -d sdk ]] && git clone --no-checkout https://scm.edu-sharing.com/edu-sharing/community/edu-sharing-community-sdk.git sdk
+popd >/dev/null
 
 add() {
 	shift || exit 1
-	path="link/$1"
-	[[ ! -d "${path}" ]] && {
-		for repo in main/*/; do
-			pushd "$repo" >/dev/null || exit 1
-			echo "################################################################################"
-			echo "$repo"
-			echo "################################################################################"
-			clonePath="../../${path}/$(basename "$repo")"
-			[[ ! -d "$clonePath" ]] && {
-				git worktree add "$clonePath" -b "$1"
+	local="$1"
+	remote="$2"
+	path="link/$local"
+	[[ -d "${path}" ]] && exit 1
+	for repo in main/*/; do
+		pushd "$repo" >/dev/null || exit 1
+		echo "################################################################################"
+		echo "$repo"
+		echo "################################################################################"
 
-				pushd "../../${path}/$(basename "$repo")" >/dev/null
-				[[ -f .gitmodules ]] && {
-					git submodule init
-					git submodule update --recursive
-				}
-				popd >/dev/null
+		git fetch origin "$remote"
+
+		worktreePath="../../${path}/$(basename "$repo")"
+		[[ ! -d "$worktreePath" ]] && {
+
+			git worktree add -b "$local" --track "${worktreePath}" "origin/$remote"
+
+			pushd "../../${path}/$(basename "$repo")" >/dev/null
+			[[ -f .gitmodules ]] && {
+				git submodule init
+				git submodule update --recursive
 			}
 			popd >/dev/null
-		done
+		}
+		popd >/dev/null
+	done
 
-		echo ""
-		echo "worktree added -> $(cd . && pwd)/${path}"
-		echo ""
-	}
+	echo ""
+	echo "worktree added -> $(cd . && pwd)/${path}"
+	echo ""
 }
 
 remove() {
 	shift || exit 1
-	path="link/$1"
-	[[ -d "${path}" ]] && {
+	local="$1"
+	read -p "Are you sure you want to continue? [y/N] " answer
+	case ${answer:0:1} in
+	y | Y)
+		path="link/$local"
+		[[ ! -d "${path}" ]] && exit 1
 		for repo in main/*/; do
 			link="../../${path}/$(basename "$repo")"
 			pushd "$repo" >/dev/null || exit 1
 			echo "################################################################################"
 			echo "$repo"
 			echo "################################################################################"
-			git worktree remove "${link}"
+			git worktree remove --force "${link}"
+			git branch -d "$local"
 			popd >/dev/null
 			rm -rf "${link}"
 		done
@@ -89,50 +96,66 @@ remove() {
 		echo ""
 		echo "worktree removed -> $(cd . && pwd)/${path}"
 		echo ""
-	}
+		;;
+	*)
+		echo Canceled.
+		;;
+	esac
+}
+
+list() {
+	[[ ! -d "main" ]] && exit 1
+	for repo in main/*/; do
+		pushd "$repo" >/dev/null || exit 1
+		echo "################################################################################"
+		echo "$repo"
+		echo "################################################################################"
+		git worktree list
+		popd >/dev/null
+	done
 }
 
 prune() {
-	[[ -d "main" ]] && {
-		for repo in main/*/; do
-			pushd "$repo" >/dev/null || exit 1
-			echo "################################################################################"
-			echo "$repo"
-			echo "################################################################################"
-			git worktree prune
-			echo ""
-			echo "worktrees pruned."
-			echo ""
-			popd >/dev/null
-		done
-	}
+	[[ ! -d "main" ]] && exit 1
+	for repo in main/*/; do
+		pushd "$repo" >/dev/null || exit 1
+		echo "################################################################################"
+		echo "$repo"
+		echo "################################################################################"
+		git worktree prune
+		popd >/dev/null
+	done
 }
 
 build() {
 	shift || exit 1
-	path="link/$1"
-	[[ -d "${path}" ]] && {
-		for repo in "${repos[@]}"
-    do
-			pushd "${path}/${repo}" >/dev/null || exit 1
-			echo "################################################################################"
-			echo "$repo"
-			echo "################################################################################"
-    	mvn $OPTS install
-    	popd >/dev/null
-    done
-	}
+	local="$1"
+	path="link/$local"
+	[[ ! -d "${path}" ]] && exit 1
+	for repo in "${repos[@]}"
+	do
+		pushd "${path}/${repo}" >/dev/null || exit 1
+		echo "################################################################################"
+		echo "$repo"
+		echo "################################################################################"
+		mvn $OPTS clean install
+		popd >/dev/null
+	done
 }
 
 case "$1" in
 add)
 	add "$@"
+#	build "$@"
 	;;
 remove)
 	remove "$@"
 	;;
+list)
+	list
+	;;
 prune)
-	prune "$@"
+	prune
 	;;
 build)
 	build "$@"
@@ -140,11 +163,12 @@ build)
 *)
 	{
 		echo "options:"
-		echo "  add <worktree>"
-		echo "  remove <worktree>"
+		echo "  add    <local-branch> <remote-branch>"
+		echo "  remove <local-branch>"
+		echo "  list"
 		echo "  prune"
 		echo ""
-		echo "  build <worktree>"
+		echo "  build <local-branch>"
 	} >&2
 	exit 1
 	;;
